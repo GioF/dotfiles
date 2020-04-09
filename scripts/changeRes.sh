904e8e6f98ddd285697781641a05a8e931c1a78c
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-04-08 23:52:17 -0300

    finally fixed my screen lmao

--- scripts/changeRes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-xrandr --newmode "1368x768_60.00" 85.25 1368 1440 1576 1784 768 771 781 798 -hsync +vsync
-xrandr --addmode VGA-1 1368x768_60.00
-xrandr --output VGA-1 --mode 1368x768_60.00
-ibus-daemon --xim &
-sleep 1
-compton &
